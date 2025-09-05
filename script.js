0177315f08701e40d3500df38cf65abddb13da13
jimmy-guzman <hi@jimmy.codes>
2025-09-05 14:29:10 -0500

    style: 🎨 prettify everything

--- script.js
+++ script.js
@@ -3,19 +3,22 @@ const products = [
     id: "A1001",
     name: "Minecraft - Xbox One",
     price: 29.99,
-    image: "https://target.scene7.com/is/image/Target/GUEST_780d12cc-79ae-474b-a4a3-0de8477746f5",
+    image:
+      "https://target.scene7.com/is/image/Target/GUEST_780d12cc-79ae-474b-a4a3-0de8477746f5",
   },
   {
     id: "A1002",
     name: "Star Wars - Xbox One",
     price: 29.99,
-    image: "https://target.scene7.com/is/image/Target/GUEST_ae8ed07e-fca7-47fb-9210-c4dbd739f6ec",
+    image:
+      "https://target.scene7.com/is/image/Target/GUEST_ae8ed07e-fca7-47fb-9210-c4dbd739f6ec",
   },
   {
     id: "A1003",
     name: "Sour Patch Kids",
     price: 1.29,
-    image: "https://target.scene7.com/is/image/Target/GUEST_421807fd-cbe0-4b54-8e86-ccaed1dfca4d",
+    image:
+      "https://target.scene7.com/is/image/Target/GUEST_421807fd-cbe0-4b54-8e86-ccaed1dfca4d",
   },
 ];
 
@@ -41,27 +44,36 @@ function createCartItemHTML(cartItem) {
 
 function displayProducts() {
   const productListContainer = document.getElementById("product-list");
+
   productListContainer.innerHTML = "";
 
   products.forEach((product) => {
     const productCard = document.createElement("div");
+
     productCard.classList.add("product-card");
     productCard.innerHTML = createProductHTML(product);
     productListContainer.appendChild(productCard);
   });
 
   document.querySelectorAll(".add-to-cart-button").forEach((button) => {
-    button.addEventListener("click", () => addToCart(button.dataset.id));
+    button.addEventListener("click", () => {
+      addToCart(button.dataset.id);
+    });
   });
 }
 
 function addToCart(productId) {
-  const existingCartItem = cart.find((item) => item.id === productId);
+  const existingCartItem = cart.find((item) => {
+    return item.id === productId;
+  });
 
   if (existingCartItem) {
     existingCartItem.quantity++;
   } else {
-    const product = products.find((item) => item.id === productId);
+    const product = products.find((item) => {
+      return item.id === productId;
+    });
+
     cart.push({
       id: product.id,
       name: product.name,
@@ -75,7 +87,9 @@ function addToCart(productId) {
 }
 
 function removeFromCart(productId) {
-  const cartItemIndex = cart.findIndex((item) => item.id === productId);
+  const cartItemIndex = cart.findIndex((item) => {
+    return item.id === productId;
+  });
 
   if (cartItemIndex > -1) {
     if (cart[cartItemIndex].quantity > 1) {
@@ -98,6 +112,7 @@ function updateCart() {
   if (cart.length === 0) {
     emptyCartMessage.style.display = "block";
     cartTotalContainer.innerHTML = "";
+
     return;
   }
 
@@ -117,7 +132,9 @@ function updateCart() {
     removeButton.classList.add("cart-remove", "button");
     removeButton.textContent = "Remove";
     removeButton.dataset.id = cartItem.id;
-    removeButton.addEventListener("click", () => removeFromCart(cartItem.id));
+    removeButton.addEventListener("click", () => {
+      return removeFromCart(cartItem.id);
+    });
 
     cartItemElement.appendChild(cartItemDetails);
     cartItemElement.appendChild(removeButton);
